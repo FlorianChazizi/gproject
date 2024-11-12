d0a0e6d3a645ce53716e1905b78203200cafec8c
FlorianChazizi <chazizi.florian@gmail.com>
2024-11-12 13:24:10 +0200

    css name changed

--- obj/Release/net8.0/gproject.AssemblyInfo.cs
+++ obj/Release/net8.0/gproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237b009eff0e5eff68c29d8240d9867c5ce376d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b442474993a7a5489280b6d4e4313682db042c27")]
 [assembly: System.Reflection.AssemblyProductAttribute("gproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
